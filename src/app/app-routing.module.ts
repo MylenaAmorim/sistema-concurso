59aab18fbd6c557a93b6b16121e22ac5df0f5db4
Mylena Amorim <mylenaamorim13@gmail.com>
2023-05-19 08:20:48 -0300

    ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormApplicationComponent } from './features/components/form-application/form-application.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './features/components/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'concurso', component: FormApplicationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
