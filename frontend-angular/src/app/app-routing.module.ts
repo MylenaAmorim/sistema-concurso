05bee5fa92a9fee7521ce103444dab461cb5f628
Mylena Amorim <mylenaamorim13@gmail.com>
2023-05-19 10:11:49 -0300

    Revert "feat: Adição frontend angular"
    This reverts commit 9b31504e8270182e3e4e4482aa5b8006a5f43005.

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FormApplicationComponent } from './features/components/form-application/form-application.component';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './features/components/home/home.component';
-
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'concurso', component: FormApplicationComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -1,47 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import { FormApplicationComponent } from './features/components/form-application/form-application.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ContestService } from './core/services/contest.service';
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderMenuComponent } from './features/components/header-menu/header-menu.component';
-import { ListContestsComponent } from './features/components/list-contests/list-contests.component';
-import { HomeComponent } from './features/components/home/home.component';
-import { HelpListInfosComponent } from './shared/helpers/help-list-infos/help-list-infos.component';
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-    FormApplicationComponent,
-    HeaderMenuComponent,
-    ListContestsComponent,
-    HomeComponent,
-    HelpListInfosComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatSlideToggleModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatListModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [
-    ContestService
-  ],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
